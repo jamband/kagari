145209e69395c39f0af30e3acb1a92d9f63a2837
Tomoki Morita <tmsongbooks215@gmail.com>
2025-04-08 20:15:51 +0900

    improve csrf.test.ts

--- packages/api/src/handlers/csrf.test.ts
+++ packages/api/src/handlers/csrf.test.ts
@@ -1,36 +1,74 @@
 import { Hono } from "hono";
 import { describe, expect, test } from "vitest";
 import { WEB_URL } from "../env.js";
-import type { InvalidResponseBody } from "../types/response.js";
 import csrf from "./csrf.js";
-import error from "./error.js";
 
-describe("csrf", () => {
-  const app = new Hono()
-    .onError(error)
-    .use(csrf)
-    .post("/foo", async (c) => {
-      return c.json({ message: "created" }, 201);
-    });
+describe("403", () => {
+  const app = new Hono().use(csrf).post("/foo", async (c) => {
+    return c.json({ message: "created" }, 201);
+  });
 
-  test("invalid", async () => {
+  test("without some headers", async () => {
     const res = await app.request("/foo", {
       method: "POST",
     });
     expect(res.status).toBe(403);
-    expect(await res.json()).toEqual<InvalidResponseBody>({
-      message: "forbidden",
-    });
   });
 
-  test("valid", async () => {
+  test("with content-type: application/x-www-form-urlencoded", async () => {
+    const res = await app.request("/foo", {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    expect(res.status).toBe(403);
+  });
+
+  test("with cross origin and content-type: application/x-www-form-urlencoded", async () => {
+    const res = await app.request(`${WEB_URL.replace(":3000", ":8000")}/foo`, {
+      method: "POST",
+      headers: {
+        origin: WEB_URL.replace(":3000", ":8000"),
+        "content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    expect(res.status).toBe(403);
+  });
+});
+
+describe("201", () => {
+  const app = new Hono().use(csrf).post("/foo", async (c) => {
+    return c.json({ message: "created" }, 201);
+  });
+
+  test("with content-type: application/json", async () => {
+    const res = await app.request("/foo", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+    });
+    expect(res.status).toBe(201);
+    expect(await res.json()).toEqual({ message: "created" });
+  });
+
+  test("with origin: WEB_URL", async () => {
     const res = await app.request("/foo", {
       method: "POST",
       headers: { origin: WEB_URL },
     });
     expect(res.status).toBe(201);
-    expect(await res.json()).toEqual({
-      message: "created",
+    expect(await res.json()).toEqual({ message: "created" });
+  });
+
+  test("with same origin and content-type: application/x-www-form-urlencoded", async () => {
+    const res = await app.request(`${WEB_URL}/foo`, {
+      method: "POST",
+      headers: {
+        origin: WEB_URL,
+        "content-type": "application/x-www-form-urlencoded",
+      },
     });
+    expect(res.status).toBe(201);
+    expect(await res.json()).toEqual({ message: "created" });
   });
 });
