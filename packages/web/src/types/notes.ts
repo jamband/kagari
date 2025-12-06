52b925c596112d7226340c99e795e844e18fd41c
Tomoki Morita <tmsongbooks215@gmail.com>
2025-12-06 14:31:00 +0900

    fix some types

--- packages/web/src/types/notes.ts
+++ packages/web/src/types/notes.ts
@@ -1,7 +1,7 @@
 import type { InferRequestType, InferResponseType } from "hono/client";
 import type { notesClient, notesIdClient } from "../clients/notes";
 
-export type Note = InferResponseType<typeof notesIdClient.$get>;
+export type Note = InferResponseType<typeof notesIdClient.$get, 200>;
 
 export type CreateNoteRequest = InferRequestType<typeof notesClient.$post>;
 export type UpdateNoteRequest = InferRequestType<typeof notesIdClient.$put>;
